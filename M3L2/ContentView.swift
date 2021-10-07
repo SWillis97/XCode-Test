188bf637f5fde6de5957dcc35eeeb156cd6c7c1a
Samuel Willis <samuelwil1997@gmail.com>
2021-10-07 13:29:46 +0100

    Initial Commit

---
+++ M3L2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3L2
+//
+//  Created by Samuel Willis on 07/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L2/M3L2App.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2App.swift
+//  M3L2
+//
+//  Created by Samuel Willis on 07/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct M3L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
